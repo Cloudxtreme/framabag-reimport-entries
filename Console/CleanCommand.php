cc762d0d099a517b29f5161c5c717ebbef142009
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-06-12 15:15:52 +0200

    refactoring

--- Console/CleanCommand.php
+++ Console/CleanCommand.php
@@ -7,6 +7,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Yaml\Yaml;
 
 class CleanCommand extends Command
 {
@@ -21,8 +23,28 @@ class CleanCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $username = $input->getArgument('username');
-        $reimport = new Reimport($username);
-        $reimport->run();
+        $locator = new FileLocator(array(__DIR__.'/../Resources'));
+        $config = $locator->locate('config.yml');
+        $configValues = Yaml::parse(file_get_contents($config));
+
+        $handle = new \PDO('sqlite:'.$configValues['reimport']['clean']['folder'].'/poche.sqlite');
+        $handle->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        $query = $handle->prepare("SELECT * from `entries` WHERE `content` = '' OR `content` = '[unable to retrieve full-text content]' LIMIT 20;");
+        $query->execute();
+        $results = $query->fetchAll();
+
+        $progress = $this->getHelperSet()->get('progress');
+        $progress->start($output, count($results));
+
+        $reimport = new Reimport($input->getArgument('username'));
+
+        foreach ($results as $result)
+        {
+            $reimport->run($result['url']);
+            $progress->advance();
+        }
+
+        $progress->finish();
     }
 }
